670035c327429f430bdbb99f5fdbd539dd1a34b4
吴景 <wujing@wujingdeMacBook-Pro.local>
2020-03-19 17:21:21 +0800

    Initial commit

---
+++ src/test/java/com/yikeo/dbadmin/DbadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yikeo.dbadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
